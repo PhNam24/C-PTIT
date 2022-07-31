9c5f34080e72436dd4569c23b8f14d9586db324b
PhNam24 <98546157+PhNam24@users.noreply.github.com>
2022-07-31 16:48:29 +0700

    Create test.cpp
    test

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define test int t; cin >> t; while (t--)
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+int main ()
+{
+    fast
+    cout << "Nam dep trai vcl";
+    return 0;
+}
